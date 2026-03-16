8deb9e5896d3cd786c96d846268876ca13512dca
SalazarBryan13 <salazarbryan13@gmail.com>
2026-03-15 20:41:07 -0500

    feat(fase3): Agregar script de carga a modelo estrella

--- src/03_modelado.py
+++ src/03_modelado.py
@@ -4,7 +4,7 @@ def run_elt_pipeline(db_uri: str, schema_sql_path: str, elt_sql_path: str):
     """Ejecuta el pipeline para crear el Modelo Estrella."""
     
     engine = create_engine(db_uri)
-    print(f"Conectado a la base de datos '{engine.url.database}'...")
+    print(f"Conectado a la base de datos '{engine.url.database}'")
     
     # 1. Crear las tablas del Modelo Estrella
     with open(schema_sql_path, 'r', encoding='utf-8') as file:
@@ -12,7 +12,7 @@ def run_elt_pipeline(db_uri: str, schema_sql_path: str, elt_sql_path: str):
     with engine.begin() as conn:
         conn.execute(text(schema_script))
     
-    # 2. Insertar datos en las tablas del Modelo Estrella 
+    # 2. Insertar datos en las tablas del Modelo Estrella
     with open(elt_sql_path, 'r', encoding='utf-8') as file:
         elt_script = file.read()
     with engine.begin() as conn:
